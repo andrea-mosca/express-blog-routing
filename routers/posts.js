90ac7811cc30ed376891cf596275e629e66f3e7a
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-13 16:15:13 +0200

    bonus 2

--- routers/posts.js
+++ routers/posts.js
@@ -1,30 +1,34 @@
 // * IMPORTS
 const express = require("express");
 const router = express.Router();
-const {posts} = require("../data/db");
+const { posts } = require("../data/db");
 // index
-router.get('/', function (req, res) {
-res.json({
-  description : 'Lista dei posts',
-  data : posts
-});
+router.get("/", function (req, res) {
+  res.json({
+    description: "Lista dei posts",
+    data: posts,
+  });
 });
 // show
-router.get('/:id', function (req, res) {
-res.send('Dettagli dei posts ' + req.params.id);
+router.get("/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  const post = posts.find(currentPost => currentPost.id === id);
+  res.json({
+    description: "Dettagli dei posts " + id,
+    data : post
+  });
 });
 // create
-router.post('/', function (req, res) {
-res.send('Creazione nuovo post');
+router.post("/", function (req, res) {
+  res.send("Creazione nuovo post");
 });
 // update
-router.put('/:id', function (req, res) {
-res.send('Modifica integrale(sostituzione) del post ' + req.params.id);
+router.put("/:id", function (req, res) {
+  res.send("Modifica integrale(sostituzione) del post " + req.params.id);
 });
 // delete
-router.delete('/:id', function (req, res) {
-res.send('Eliminazione del post ' + req.params.id);
+router.delete("/:id", function (req, res) {
+  res.send("Eliminazione del post " + req.params.id);
 });
-
 
 module.exports = router;
